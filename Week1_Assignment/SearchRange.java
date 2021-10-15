1bd4756558e0c73c0a289fdd74dcef8296c406b6
Shubhi Yadav <77799883+crossHead@users.noreply.github.com>
2021-10-16 03:24:12 +0530

    Update SearchRange.java

--- Week1_Assignment/SearchRange.java
+++ Week1_Assignment/SearchRange.java
@@ -1,28 +1,29 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-         int x=nums.length;
-      
-       int[] de={-1,-1};
-        if(x==0){
-            return de ;
+        if(nums.length==0){
+            return new int[]{-1,-1};
         }
-          int[] r=new int[2];
-         
-        for(int i=0;i<x;i++){
-            if(nums[i]==target){
-                r[0]=i;
-               
-                break;
-            }
-            
-            return r;
+        int[] res=new int[2];
+        int start=0, end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(nums[mid]>=target) end=mid-1;
+            else start=mid+1;
         }
-         for(int j=r[0]+1;j<x;j++){
-                    if(nums[j]!=target){
-r[1]=j-1;
-                      return r;
-                    }
-                }
-        return  de;
+        if(start>nums.length-1||nums[start]!=target)
+            return new int[]{-1,-1};
+
+        res[0]=start;
+
+        int start2=0, end2=nums.length-1;
+        while(start2<=end2){
+            int mid=start2+(end2-start2)/2;
+
+            if(nums[mid]<=target) start2=mid+1;
+            else  end2=mid-1;
+        }
+        res[1]=end2;
+        return res;
     }
 }
